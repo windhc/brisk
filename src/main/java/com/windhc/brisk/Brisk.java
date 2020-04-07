241d43dc1d4d85422066a9bbb5c115b2f405b720
windhc <1440557925@qq.com>
2020-04-07 18:46:08 +0800

    add logger

--- src/main/java/com/windhc/brisk/Brisk.java
+++ src/main/java/com/windhc/brisk/Brisk.java
@@ -1,9 +1,13 @@
 package com.windhc.brisk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author windhc
  */
 public class Brisk {
+    private static final Logger logger = LoggerFactory.getLogger(Brisk.class);
 
     private Brisk() {
     }
