af5ec28a9bd68bce4c9556f2caceed7fcff5080d
windhc <1440557925@qq.com>
2020-07-05 23:31:39 +0800

    add mvc annotation and bean annotation

--- src/main/java/com/windhc/brisk/mvc/annotation/PathParam.java
+++ src/main/java/com/windhc/brisk/mvc/annotation/PathParam.java
@@ -2,6 +2,9 @@ package com.windhc.brisk.mvc.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * @author windhc
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
